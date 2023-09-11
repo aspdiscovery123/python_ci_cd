6b79d572807c4560abefb00669cbf3e0122992c2
aspdiscovery123 <48548207+aspdiscovery123@users.noreply.github.com>
2023-09-11 15:54:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-print("hello world")
+from flask import Flask
 
-print("hey")
+app = Flask(__name__)
 
-print("hellow world application")
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
